4a150af347b21e2269331a68f44f2a5ae7062b45
Joseph Hunkeler <jhunkeler@users.noreply.github.com>
2020-07-02 22:20:04 -0400

    Update test_usage.sh

--- tests/test_usage.sh
+++ tests/test_usage.sh
@@ -1,9 +1,29 @@
-test_splitfits_usage_longopt() {
+test_splitfits_usage_longopt_help() {
     ${test_program} --help
     return $?
 }
 
-test_splitfits_usage_shortopt() {
+test_splitfits_usage_shortopt_help() {
     ${test_program} -h
     return $?
+}
+
+test_splitfits_usage_longopt_version() {
+    result=$(${test_program} --version)
+    retval=$?
+    
+    if [[ -z $result ]]; then
+        return 1
+    fi
+    return $retval
+}
+
+test_splitfits_usage_shortopt_version() {
+    result=$(${test_program} -V)
+    retval=$?
+    
+    if [[ -z $result ]]; then
+        return 1
+    fi
+    return $retval
 }
