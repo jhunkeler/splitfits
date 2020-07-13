dc6dcc66e8ad0d768c6fc0c1c32bd31e95060088
Joseph Hunkeler <jhunkeler@gmail.com>
2020-07-13 18:25:58 -0400

    Update nexus config variables

--- test_config.sh
+++ test_config.sh
@@ -1,9 +1,10 @@
+NEXUS_URL=https://nx.astroconda.org
+NEXUS_AUTH=${NEXUS_AUTH:-}
 test_program="${RTDIR}/splitfits"
 test_program_version="$(git describe --always --tags --long)"
 test_data="${RTDIR}/data"
-test_data_remote=https://nx.astroconda.org/repository
-test_data_remote_auth=${test_data_remote_auth:-}
-test_data_upload="generic/spb-splitfits"
+test_data_upload="repository/generic/spb-splitfits"
+test_dep_nexus_bash_rev=a2586e952bb9e5e70a2d62c55337574d7086b9a0
 if [[ $CIRCLECI == "true" ]]; then
     test_data_upload="${test_data_upload}/ci/${CIRCLE_BRANCH}_${CIRCLE_JOB}/${CIRCLE_BUILD_NUM}/${test_program_version}"
 else
