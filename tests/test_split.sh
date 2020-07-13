5245975f67212fdae08033e13c0c7d2b6db1256e
Joseph Hunkeler <jhunkeler@gmail.com>
2020-07-13 18:26:21 -0400

    Update test code

--- tests/test_split.sh
+++ tests/test_split.sh
@@ -1,7 +1,7 @@
 test_splitfits_data1() {
     local datafile
 
-    datafile=$(get_data generic/fits/sample.fits)
+    datafile=$(nexus_raw_download repository/generic/fits/sample.fits)
     [[ ! -f ${datafile} ]] && return 1
 
     if ! ${test_program} -o ${test_case} ${datafile}; then
@@ -14,7 +14,7 @@ test_splitfits_data1() {
 test_splitfits_combine_data1() {
     local datafile
 
-    datafile=$(get_data generic/fits/sample.fits)
+    datafile=$(nexus_raw_download repository/generic/fits/sample.fits)
     [[ ! -f ${datafile} ]] && return 1
 
     if ! ${test_program} -o ${test_case} ${datafile}; then
